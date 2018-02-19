5465aff092b76e8811233577def3e17e214b5f6c
s.guerrier <s.guerrier@criteo.com>
2018-02-19 17:30:02 +0100

    added the query to infoconcert and return reservation link

--- popup.js
+++ popup.js
@@ -1,12 +1,34 @@
 chrome.runtime.onMessage.addListener(function(request, sender) {
   if (request.action == "getSource") {
-    message.innerText = request.source;
+    var artist = request.source;
+    message.innerText = artist;
+    //ret.innerText = artist;
+    getHTMLFromInfoconcert(artist);
   }
 });
+
+
+function getHTMLFromInfoconcert(artist) {
+  var url = "http://www.infoconcert.com/recherche-concert-avancee.html?motclef_artiste=" + artist + "&motclef_ville=Paris";
+  //ret.innerText = url;
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function() {
+    var response = xhr.responseText;
+    var parser = new DOMParser();
+    var doc = parser.parseFromString(response, "text/html");
+    //ghet button reservez in order to take only bookable shows
+    var links = doc.getElementsByClassName("btn btn_reservez png2");
+    //display reservation link
+    ret.innerText = links.item(0).getAttribute("href");
+  }
+  xhr.open("GET", url, true);
+  xhr.send();
+}
 
 function onWindowLoad() {
 
   var message = document.querySelector('#message');
+  var ret = document.querySelector('#ret');
 
   chrome.tabs.executeScript(null, {
     file: "getArtistName.js"
